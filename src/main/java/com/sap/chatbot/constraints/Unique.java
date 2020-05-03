81cde7760041c8d611865cee1c7b3314c0f25df2
Deian Petrov <deian.petrov@sap.com>
2020-05-03 11:04:18 +0300

    remove unused

--- src/main/java/com/sap/chatbot/constraints/Unique.java
+++ src/main/java/com/sap/chatbot/constraints/Unique.java
@@ -1,30 +1,19 @@
 package com.sap.chatbot.constraints;
 
-import java.lang.annotation.Documented;
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-
-import javax.validation.Constraint;
-import javax.validation.Payload;
-
-import com.sap.chatbot.constraints.validator.UniqueValidator;
-
 /**
  * @author Florin-Gabriel Barbuceanu, florin.barbuceanu@sap.com
  * @since 23/05/2018
  */
-@Documented
-@Constraint(validatedBy = UniqueValidator.class)
-@Target({ElementType.TYPE})
-@Retention(RetentionPolicy.RUNTIME)
-public @interface Unique {
-  String message() default "There can be only one.";
-
-  Class<?>[] groups() default {};
-
-  Class<? extends Payload>[] payload() default {};
-
-  String value() default "";
-}
+//@Documented
+//@Constraint(validatedBy = UniqueValidator.class)
+//@Target({ElementType.TYPE})
+//@Retention(RetentionPolicy.RUNTIME)
+//public @interface Unique {
+//  String message() default "There can be only one.";
+//
+//  Class<?>[] groups() default {};
+//
+//  Class<? extends Payload>[] payload() default {};
+//
+//  String value() default "";
+//}
--- src/main/java/com/sap/chatbot/constraints/validator/UniqueValidator.java
+++ src/main/java/com/sap/chatbot/constraints/validator/UniqueValidator.java
@@ -1,26 +1,20 @@
 package com.sap.chatbot.constraints.validator;
 
-import javax.validation.ConstraintValidator;
-import javax.validation.ConstraintValidatorContext;
-
-import com.sap.chatbot.constraints.Unique;
-import com.sap.chatbot.forms.EmployeeCreationForm;
-
 /**
  * @author Florin-Gabriel Barbuceanu, florin.barbuceanu@sap.com
  * @since 23/05/2018
  */
-public class UniqueValidator implements ConstraintValidator<Unique, EmployeeCreationForm> {
-
-  private Unique uniqueConstraint;
-
-  @Override
-  public void initialize(Unique constraintAnnotation) {
-    this.uniqueConstraint = constraintAnnotation;
-  }
-
-  @Override
-  public boolean isValid(EmployeeCreationForm value, ConstraintValidatorContext context) {
-    return !uniqueConstraint.value().equalsIgnoreCase(value.getName());
-  }
-}
+//public class UniqueValidator implements ConstraintValidator<Unique, EmployeeCreationForm> {
+//
+//  private Unique uniqueConstraint;
+//
+//  @Override
+//  public void initialize(Unique constraintAnnotation) {
+//    this.uniqueConstraint = constraintAnnotation;
+//  }
+//
+//  @Override
+//  public boolean isValid(EmployeeCreationForm value, ConstraintValidatorContext context) {
+//    return !uniqueConstraint.value().equalsIgnoreCase(value.getName());
+//  }
+//}
